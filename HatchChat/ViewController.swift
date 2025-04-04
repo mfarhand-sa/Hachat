d85f57bcfe09ccff61daf1931fecf32105350102
MoLowKey <hello@mohi.dev>
2025-04-03 23:56:34 -0700

    Initial Commit

---
+++ HatchChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HatchChat
+//
+//  Created by Mohi Farhand on 2025-04-03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
